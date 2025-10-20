f2e2f49260510101947e22d67c85cdf135f8e3f6
Mustafa Orhan <swietcher@Mustafa-MacBook-Pro.local>
2025-10-20 19:23:57 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ahir-book/sw.js', { scope: '/ahir-book/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
